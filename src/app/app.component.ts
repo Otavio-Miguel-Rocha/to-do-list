cf3ae92ae302afde4235a2ef4c799a4df64ecef6
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-04-27 22:01:24 -0300

    Adiciona o to-do-list quase pronto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+interface Tarefa {
+  codigoTarefa: number;
+  nome: string;
+  categoria: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log(localStorage.getItem("Tarefas"));
+  }
+  tarefas: Tarefa[] = [];
+  codigo: number = 0;
+
+  tarefa: Tarefa = {
+    codigoTarefa: 0,
+    nome: "",
+    categoria: "",
+  };
+
+  cadastrarUsuario(): void {
+    const tarefa: Tarefa = {
+      codigoTarefa: this.codigo++,
+      nome: this.tarefa.nome,
+      categoria: this.tarefa.categoria,
+    };
+
+    this.tarefas.push(tarefa);
+    localStorage.setItem("Tarefas", JSON.stringify(this.tarefas));
+    this.tarefa.nome = "";
+  }
+  remover(codigo: number): void {
+    this.tarefas.splice(codigo, 1);
+  }
 }
