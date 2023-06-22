a2a8ae046348c494a5588e75df1047e454b3390f
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-06-22 16:54:41 -0300

    Adiciona a navegação e começa a estilização da tela inicial(login e register)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/users/user';
+import { UserRepository } from 'src/repositories/user.repository';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  private userId: string = 'henrique.santos';
+  private users: User[] = [];
+  user: User;
+
+  constructor(
+    private userRepository: UserRepository
+  ) {
+    this.users = this.userRepository.getUsers();
+    this.user = this.getUsuarioLogado();
+    console.log(this.user);
+  }
+
+  adicionarTarefa(): void {
+    if (!this.hasPermission('Add')) {
+      alert('Não pode cadastrar');
+      return;
+    }
+    alert('Pode cadastrar');
+  }
+
+  editarTarefa(): void {
+    if (!this.hasPermission('Edit')) {
+      alert('Não pode cadastrar');
+      return;
+    }
+    alert('Pode cadastrar');
+  }
+
+  removerTarefa(): void {
+    if (!this.hasPermission('Remove')) {
+      alert('Não pode cadastrar');
+      return;
+    }
+    alert('Pode cadastrar');
+  }
+
+  hasPermission(permission: string): boolean {
+    return this.user.cardPermissions.some((cardPermission) => {
+      return cardPermission === permission;
+    });
+  }
+
+  private getUsuarioLogado(): User {
+    return this.users.find((user) => {
+      return user.id === this.userId
+    }) as User;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserRepository } from 'src/repositories/user.repository';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent,
+    NavegacaoComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    UserRepository
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
